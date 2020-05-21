3395a011c602326e07f4cd9cc8bdc51ed7691a34
jing wang2 <jing.wang2@ninebot.com>
2020-05-21 14:19:26 +0800

    [app]add gradle config

--- app/src/main/java/com/will/androidstudy/MainActivity.java
+++ app/src/main/java/com/will/androidstudy/MainActivity.java
@@ -3,9 +3,12 @@ package com.will.androidstudy;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "AndroidStudy";
 
     // Used to load the 'native-lib' library on application startup.
     static {
@@ -19,7 +22,10 @@ public class MainActivity extends AppCompatActivity {
 
         // Example of a call to a native method
         TextView tv = findViewById(R.id.sample_text);
-        tv.setText(stringFromJNI());
+        tv.setText(BuildConfig.algo_vision);
+        for(String temp : BuildConfig.algo_vision_array) {
+            Log.i(TAG, "onCreate " + temp);
+        }
     }
 
     /**
